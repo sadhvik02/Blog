99906b28409a4e6111cca6a300b33100808cd3fa
sadhvik02 <sadhviknayakwadi02@gmail.com>
2025-05-16 16:06:09 +0530

    First Commit

---
+++ src/BlogPostList.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import BlogPostItem from './BlogPostItem';
+import styles from './BlogPostList.module.css';
+
+const BlogPostList = ({ posts }) => {
+  if (posts.length === 0) {
+    return <p className={styles.noPosts}>No blog posts available.</p>;
+  }
+
+  return (
+    <div className={styles.blogPostList}>
+      {posts.map((post) => (
+        <BlogPostItem
+          key={post.id}
+          id={post.id}
+          title={post.title}
+          summary={post.summary}
+          date={post.date}
+          url={post.url}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default BlogPostList;
